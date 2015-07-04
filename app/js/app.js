1b93f622c89bf28f3293a3211c5a01629784f531
Nunin Alexey <webdesc@gmail.com>
2015-07-04 21:00:07 +0300

    add accordeon and up button

--- app/js/app.js
+++ app/js/app.js
@@ -13,9 +13,84 @@ var viewer = (function(){
 	};
 
 	function toggleViewCatalog(){
-		var setView = $(this).find('option:checked').data('view'),
+		var setView   = $(this).find('option:checked').data('view'),
 			classList = catalogList.attr('class').split(/\s+/);
-		catalogList.removeClass(classList[1]).addClass('catalog-list_view-' + setView);
-	}
+
+		catalogList
+			.removeClass(classList[1])
+			.addClass('catalog-list_view-' + setView);
+	};
+
+})();
+
+
+var accordeon = (function(){
+
+	var accordeon   = $('.menu-side__item_subcat'),
+		subCatLinks = $('.menu-side__item_subcat > .menu-side__link'),
+		allSubMenu  = accordeon.find('.menu-side__submenu');
+
+	return {
+		init: function(){
+			this.setupListener();
+		},
+		setupListener: function(){
+			subCatLinks.on('click', toggleSubMenu);
+		}
+	};
+
+	function toggleSubMenu(e) {
+		e.preventDefault();
+		var that    = $(this),
+			subMenu = that.parent().find('.menu-side__submenu');
+
+		// скрываем подменю всех соседей
+		subMenu
+			.end()
+			.siblings(accordeon)
+			.find(allSubMenu)
+			.slideUp();
+
+		// показываем текущее подменю
+		subMenu
+			.stop()
+			.slideToggle();
+
+		subCatLinks.removeClass('menu-link_active');
+		that.addClass('menu-link_active');
+	};
+
+})();
+
+
+var up = (function(){
+
+	var win        = $(window),
+		doc        = $(document),
+		upLink     = $('.up'),
+		scrollPage = 200,
+		speedUp    = 400;
+
+	return {
+		init: function(){
+			this.setupListener();
+		},
+		setupListener: function(){
+			upLink.on('click', scrollUp);
+			win.on('scroll', toggleShowUp);
+		}
+	};
+
+	function toggleShowUp(){
+		if (doc.scrollTop() > scrollPage) {
+			upLink.fadeIn(300);
+		} else {
+			upLink.fadeOut(300);
+		}
+	};
+
+	function scrollUp(){
+		$('html, body').animate({scrollTop:0}, speedUp);
+	};
 
 })();
--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
 
+	up.init();
+
 	if ($('.catalog-select-view').length) {
 		viewer.init();
-	}
+	};
+	
+	if ($('.menu-side__item_subcat').length) {
+		accordeon.init();
+	};
 
 });
